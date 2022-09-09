61e33c742d15606e93f3f21905ebe6b481e3bebd
sebastian200188 <sebastiantabaresrozo2001@gmail.com>
2022-09-09 16:52:08 -0500

    add config db

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const routes = require('./routes')
 const app = express()
 app.set('port', process.env.PORT || 9000)
 const dbOptions = {
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'root123',
-    database: 'form'
+    host: 'us-cdbr-east-06.cleardb.net',
+    //port: '3306',
+    user: 'ba47b630ca63e5',
+    password: '43f82781',
+    database: 'heroku_9dab5d1d4fe89e1'
 }
+
+// mysql://ba47b630ca63e5:43f82781@us-cdbr-east-06.cleardb.net/heroku_9dab5d1d4fe89e1?reconnect=true
 
 app.use(myconn(mysql, dbOptions, 'single'))
 app.use(express.json())
